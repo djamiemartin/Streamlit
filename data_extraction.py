0be121f5c329b5ce713d2d9371c8b551de7fa198
David Ramos <davidramossal@gmail.com>
2024-10-21 16:52:07 +0300

    download_folder now supports folders inside folders

--- data_extraction.py
+++ data_extraction.py
@@ -2,27 +2,29 @@ import os
 
 from google.cloud import storage
 
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'micro-root-379316-a3e3cb0be532.json'
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "micro-root-379316-a3e3cb0be532.json"
 
 storage_client = storage.Client()
 
 dir(storage_client)
 
+
 def download_file_from_bucket(blob_name, file_path, bucket_name):
     try:
         bucket = storage_client.get_bucket(bucket_name)
         blob = bucket.blob(blob_name)
-        with open(file_path, 'wb') as f:
+        with open(file_path, "wb") as f:
             storage_client.download_blob_to_file(blob, f)
         return True
     except Exception as e:
         print(e)
         return False
 
-bucket_name = 'predict-maintenanceproject'
+
 # download_file_from_bucket('Predictive/Predictive-csv1/c1/c1/c_1_001.csv', os.path.join(os.getcwd(), 'c_1_001.csv'), bucket_name)
 
 # create a function to download a folder from google cloud bucket
+
 
 def download_folder(bucket_name, prefix, local_folder):
     # Create a storage client
@@ -37,8 +39,11 @@ def download_folder(bucket_name, prefix, local_folder):
     # Download each file in the folder
     for blob in blobs:
         # Get the relative path within the folder
-    
-        relative_path = blob.name[len(prefix):]
+        relative_path = blob.name[len(prefix) :]
+
+        # If relative_path is empty or represents a directory, skip it
+        if not relative_path or relative_path.endswith("/"):
+            continue
 
         # Create local destination path
         local_path = os.path.join(local_folder, relative_path)
@@ -50,9 +55,8 @@ def download_folder(bucket_name, prefix, local_folder):
         blob.download_to_filename(local_path)
         print(f"Downloaded {blob.name} to {local_path}")
 
-prefix =   'Predictive/Predictive-csv1/c1/c1/'    #'your-folder-prefix/'  # Folder in the bucket
-local_folder =  'data/c1/c1'                            #'/local/destination/folder'
+
+bucket_name = "predict-maintenanceproject"
+prefix = "dashboard/"  #'your-folder-prefix/'  # Folder in the bucket
+local_folder = "data/dashboard/"  #'/local/destination/folder'
 download_folder(bucket_name, prefix, local_folder)
-
-
-
